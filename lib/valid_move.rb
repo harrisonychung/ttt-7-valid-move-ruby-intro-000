917dc02e15355501ba367d19c756555539f48d6a
Harrison Chung <harrison.y.chung@gmail.com>
2020-08-28 18:24:30 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -19,7 +19,7 @@ def valid_move?(board, index)
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-if position_taken?(board, index) == false && on_the_board?(index) == true
+if position_taken?(board, index) == false && on_the_board?(ind) == true
   return true
 else
   return false
